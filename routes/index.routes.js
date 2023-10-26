182db179bef814c3339bec2b688227e5288fb5cd
“Nicole <itsnicoleduffy@gmail.com>
2023-10-26 18:32:05 +0200

    she finally has a working autho

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -14,4 +14,22 @@ router.get("/users", async (req, res) => {
   }
 });
 
+router.get("/songs", async (req, res) => {
+  try {
+    const newSong = await Song.find();
+    res.status(201).json(newSong);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+router.get("/artists", async (req, res) => {
+  try {
+    const newArtist = await Artist.find();
+    res.status(201).json(newArtist);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
 module.exports = router;
