11cace1b69b6c76ef8f415158e464b26de42b7ee
“Nicole <itsnicoleduffy@gmail.com>
2023-10-27 18:46:17 +0200

    front and back connected

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const PORT = 5005;
-require("dotenv").config()
-require("./db/index")
+require("dotenv").config();
+require("./db/index");
 
 const app = express();
-require("./config")(app)
+require("./config")(app);
 
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
